345a48129a0bb6379dd7a90bfce63cea96d189e8
Roskovskiy <bazanov4@gmail.com>
2023-01-24 16:32:25 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user ="Ivan";
+
+alert(`Привет,${user}`);
+
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(answer+ 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+
+// const arr = ['a', 'b', 'c'];
+// const arrObj = {
+//     a: 'a',
+//     '1': 'b',
+//     2: 'c',
+//     abc: {
+
+//     }
+// }
+
+// const b = 'b';
+
+// arrObj[b] = '1234';
+
+// // arrObj.b = '1234';
+// arrObj['b'] = '1234';
+
+// console.log(arrObj['b']);
+// console.log(arrObj.b);
+
+// const storeName= 'Magnit';
+// const storeDescription ={
+//     budget:  10000,
+//     employees:['Andrey','Kostyan','Gena'],
+//     products: {
+//         'potato': 45,
+//         'apple': 60
+//     },
+//     open: true   
+// }
+
+
